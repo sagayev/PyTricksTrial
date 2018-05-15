6a3c89aa534b8f5efc5edd4c212b7ade9332820d
Samir Ağayev <Samir_Agayev@cbar.az>
2018-05-15 10:06:01 +0400

    f strings in class methods str and repr

--- f_strings_rp.py
+++ f_strings_rp.py
@@ -25,3 +25,32 @@ def to_lowercase(input):
 name = "Samir Aghayev"
 message = f"{to_lowercase(name)} is funny"
 print (message)
+
+
+class Comedian:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age =age
+        
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}."
+    
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}. Surprise!"
+    
+new_comedian = Comedian("Samir", "Aghayev", "40")
+
+message=f"{new_comedian}"  #default way of referring to __str__ method
+print(message)
+#>>>Samir Aghayev is 40
+
+message=f"{new_comedian!r}"  #How to refer to __repr__ method
+print(message)
+#>>>Samir Aghayev is 40. Surprise!
+
+
+"""
+Multiline f-strings
+
+"""
